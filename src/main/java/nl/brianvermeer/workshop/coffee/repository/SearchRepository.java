db7b4117c8bfc301a818e46e353a0e5fe160bd92
Pedro Martins <plmartins@criticalsoftware.com>
2023-06-07 14:43:10 +0100

    Assigment 1

--- src/main/java/nl/brianvermeer/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/nl/brianvermeer/workshop/coffee/repository/SearchRepository.java
@@ -1,28 +1,30 @@
 package nl.brianvermeer.workshop.coffee.repository;
 
+import java.util.List;
+import java.util.Locale;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 import nl.brianvermeer.workshop.coffee.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import java.util.List;
-import java.util.Locale;
-
 @Repository
 public class SearchRepository {
 
-    @Autowired
-    EntityManager em;
+  @Autowired
+  EntityManager em;
 
-    @Autowired
-    DataSource dataSource;
+  @Autowired
+  DataSource dataSource;
 
-    public List<Product> searchProduct (String input) {
-        var lowerInput = input.toLowerCase(Locale.ROOT);
-        String query = "Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'";
-        var resultList = (List<Product>) em.createNativeQuery(query, Product.class).getResultList();
-        return resultList;
-    }
+  public List<Product> searchProduct(String input) {
+    var lowerInput = input.toLowerCase(Locale.ROOT);
+    String query = "Select * from Product where lower(description) like ?1  OR lower(product_name) like ?2";
+
+    var q = em.createNativeQuery(query, Product.class);
+    q.setParameter(1, "%" + lowerInput + "%");
+    q.setParameter(2, "%" + lowerInput + "%");
+    return q.getResultList();
+  }
 
 }
